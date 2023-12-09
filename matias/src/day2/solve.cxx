42bab20a62a51536272b6510ef4c025a4ff66e9b
Matias Larsson <matias.larsson@gmail.com>
2023-12-09 17:02:07 +0100

    Matias, Day 2, part 2

--- matias/src/day2/solve.cxx
+++ matias/src/day2/solve.cxx
@@ -3,6 +3,7 @@
 #include <string>
 #include <sstream>
 #include <utility>
+#include <stdexcept>
 
 struct Bag {
     unsigned reds;
@@ -18,6 +19,25 @@ struct Bag {
             return blues;
         }
         return 0;
+    }
+
+    void leastCubeCount(std::string color, unsigned count) {
+        if (color == "red") {
+            if (reds < count) {
+                reds = count;
+            }
+        } else if (color == "green") {
+            if (greens < count) {
+                greens = count;
+            }
+        } else if (color == "blue") {
+            if (blues < count) {
+                blues = count;
+            }
+        } else {
+            using namespace std::string_literals;
+            throw std::invalid_argument("Unknown color '"s + color + "'");
+        }
     }
 };
 
@@ -61,7 +81,44 @@ static void part1(const std::string &input_filename) {
     std::cout << sum << std::flush;
 }
 
+static void part2(const std::string &input_filename) {
+    std::ifstream f(input_filename);
+    std::string line;
+
+    std::string game;
+    unsigned gameNo;
+    std::string cubeColor;
+    unsigned cubeCount;
+    char colon;
+    char commaOrSemicolon;
+
+    unsigned long sum = 0;
+
+    while (std::getline(f, line)) {
+        Bag bag{};
+
+        line += "|"; // commaOrSemicolon
+        std::stringstream ss(line);
+        ss >> game >> gameNo >> colon;
+        std::cerr << game << " " << gameNo << colon << std::flush;
+        bool applies = true;
+        do {
+            ss >> cubeCount >> cubeColor;
+            commaOrSemicolon = cubeColor.back();
+            cubeColor.pop_back();
+            std::cerr << " " << cubeCount << " " << cubeColor << commaOrSemicolon << std::flush;
+            bag.leastCubeCount(std::move(cubeColor), cubeCount);
+        } while (ss && (commaOrSemicolon == ',' || commaOrSemicolon == ';'));
+        sum += bag.reds * bag.greens * bag.blues;
+        std::cerr << std::endl << "  red=" << bag.reds << " green=" << bag.greens << " blue=" << bag.blues << std::endl;
+    }
+
+    std::cout << sum << std::flush;
+}
+
 int main() {
     part1("input");
+    std::cout << ", ";
+    part2("input");
     std::cout << std::endl;
 }
